732641c6cc75e61a7ffd69d697bfdce4bbe840cd
Umma Ahimsha <uahimsha@gmail.com>
2022-12-17 03:57:06 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,34 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+import { dependencies } from './package.json';
+
+// Packages we want in the vendor aka the deps needed in the entire app.
+const globalVendorPackages = ['react', 'react-dom', 'react-router-dom'];
+
+// Split each dependency into separate chunk
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (globalVendorPackages.includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: globalVendorPackages,
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 
   /**
    * defining aliases
